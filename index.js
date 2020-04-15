52b438f7ea0b859062438a737f3bd9dabf450f65
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-15 02:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
-var topic = "space";
+var topic = "coding and web design";
 
-var conversation = `${topic}`;
+var conversation = `Lets talk about ${topic}`;
